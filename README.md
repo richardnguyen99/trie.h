111f4b2bffebffa519425b2f618b52cd309f1daa
Richard Nguyen <richard.ng0616@gmail.com>
2023-03-21 15:54:22 -0700

    Initialize trie.h

---
+++ README.md
@@ -0,0 +1,3 @@
+# trie.h
+
+A simple implementation for the **prefix tree** data structure in C.
---
+++ trie.h
@@ -0,0 +1,7 @@
+#ifndef __TRIE_H
+#define __TRIE_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+#endif
